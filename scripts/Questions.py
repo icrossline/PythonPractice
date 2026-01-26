388ad747cbe3ac302b99c621ddb21eb4ea24b2f3
Abhishek Uniyal <abhiuni02@gmail.com>
2026-01-26 10:33:23 +0530

    Update Questions.py

--- scripts/Questions.py
+++ scripts/Questions.py
@@ -1,29 +1,29 @@
 # WA Function to check string is palindrome or not ?
 def palindrome(a):
     if a.lower() == a.lower()[::-1]:
-        return True
+        return 'Palindome'
     else:
-        return False
+        return 'Not  a palindrome'
     
-if palindrome('racecar'):
-    print('Palindrome')
-else:
-    print('Not a palindrome')
+print(palindrome('mom')) #palindrome
+print(palindrome('red')) #not a palindrome
+
+ 
 
 
 #WA function to check  palindrome in a list ?
 
 def palindromelist(a):
     for ch in a:
-        if ch == ch[::-1]:
+        if ch.lower() == ch.lower()[::-1]:
             res.append(ch)
         else:
             pass
     return res    
     
-res=[]        
+res=[] #Empty list       
 
 palindromelist(['mom','red','racecar']) 
-print(res)
+print(res) #return mom and racecar
 
 
