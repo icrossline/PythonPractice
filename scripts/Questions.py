b9609891b9c08221162c4da3f4a87a5a9479541a
Abhishek Uniyal <abhiuni02@gmail.com>
2026-01-26 11:20:45 +0530

    Update Questions.py

--- scripts/Questions.py
+++ scripts/Questions.py
@@ -27,3 +27,19 @@ palindromelist(['mom','red','racecar'])
 print(res) #return mom and racecar
 
 
+#WAP to find largest number in a given list
+
+num=[1,5,2,8,6,9,3]
+largest=num[0]
+index=0
+
+for n in  range(len(num)):
+    if largest<num[n]:
+        largest=num[n]
+        index=n
+        
+        
+print(f"The greatest number in given list is {largest} present at index no.{index}")  #The greatest number in given list is 9 present at index no.5  
+
+
+
