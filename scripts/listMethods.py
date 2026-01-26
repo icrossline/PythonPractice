78581c1ab053e52f47595f94b235a07ad3084b13
Abhishek Uniyal <abhiuni02@gmail.com>
2026-01-26 11:26:33 +0530

    Implement list manipulation methods in listMethods.py
    Added methods for list manipulation including append, insert, pop, remove, clear, reverse, sort, copy, and extend.

---
+++ scripts/listMethods.py
@@ -0,0 +1,21 @@
+numList=[1,2,3,4,5]
+
+#INSERT AND REMOVE ELEMENTS
+numList.append(6) # add element at last 
+numList.insert(6,7) # add element in a given index 6 is index 7 is the element
+numList.pop() #remove element from Last
+numList.remove(5) # remove a particular or given element
+numList.clear() # remove all elements from a list
+
+
+# Reverse And Sorting
+
+numList.reverse() # Reverse the list
+numList.sort() # sort the list A-Z or 0-9
+
+# Copy or Extend list
+newnumList= numList.copy() # Copy the list
+numList.extend(newNumList) # extends or combine list
+
+
+
